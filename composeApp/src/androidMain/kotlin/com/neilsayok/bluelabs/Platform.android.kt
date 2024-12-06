04781ae5af9f68338f514a0c06f977f781f0bf80
NeilSayok <sayokdeymajumder1998@gmail.com>
2024-12-06 22:43:23 +0530

    BAsic Changes

--- composeApp/src/androidMain/kotlin/com/neilsayok/bluelabs/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/com/neilsayok/bluelabs/Platform.android.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AssistChip
@@ -34,7 +35,8 @@ actual fun getPlatform(): Platform = AndroidPlatform()
 fun HomeCard() {
 
     Card(
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier.width(120.dp)
     ) {
 
         Column {
