6cf144cc8b392115cd365ac90e431579d58c0c5c
NeilSayok <sayokdeymajumder1998@gmail.com>
2025-05-17 17:49:58 +0530

    App Bar

--- composeApp/src/commonMain/kotlin/com/neilsayok/bluelabs/common/appbar/MainAppBar.kt
+++ composeApp/src/commonMain/kotlin/com/neilsayok/bluelabs/common/appbar/MainAppBar.kt
@@ -6,26 +6,30 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material.icons.filled.Policy
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,46 +48,39 @@ fun MainAppBar(isDark: Boolean, onThemeChange: (Boolean) -> Unit) {
 
     val scope = rememberCoroutineScope()
 
-    val textFieldState = remember { TextFieldState("") }
+    var searchKey by remember { mutableStateOf("") }
 
     TopAppBar(
         title = {
-
-            Row(horizontalArrangement = Arrangement.spacedBy(12.dp), verticalAlignment = Alignment.CenterVertically) {
-                Image(painter = if (isDark)
-                    painterResource(Res.drawable.blue_labs_icon_white)
-                    else
-                    painterResource(Res.drawable.blue_labs_icon),
-                "Bluelabs Icon"
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier.fillMaxHeight(0.6f),
+                    painter = if (isDark) painterResource(Res.drawable.blue_labs_icon_white)
+                    else painterResource(Res.drawable.blue_labs_icon),
+                    contentDescription = "Bluelabs Icon"
                 )
-                Text("BLUE LABS",
-                    style = MaterialTheme.typography.titleLarge.copy(
+                Text(
+                    "BLUE LABS", style = MaterialTheme.typography.titleLarge.copy(
                         fontWeight = FontWeight.Bold,
-                        //fontFamily = FontFamily.Monospace,
                         letterSpacing = 1.5.sp
                     )
                 )
-                SearchBar(
-                    inputField = {
-                        SearchBarDefaults.InputField(
-                            query = textFieldState.text.toString(),
-                            onQueryChange = { textFieldState.edit { replace(0, length, it) } },
-                            onSearch = {
-//                                onSearch(textFieldState.text.toString())
-//                                expanded = false
-                            },
-                            expanded = false,
-                            onExpandedChange = {  },
-                            placeholder = { Text("Search") }
-                        )
-                    },
-                    expanded = false,
-                    onExpandedChange = {}
 
+                OutlinedTextField(
+                    value = searchKey,
+                    onValueChange = { searchKey = it },
+                    modifier = Modifier.height(48.dp),
+                    textStyle = MaterialTheme.typography.labelSmall,
+                    singleLine = true,
+                    leadingIcon = {Icon(Icons.Filled.Search, "Search", tint = MaterialTheme.colorScheme.primary) },
+                    colors = OutlinedTextFieldDefaults.colors()
+                )
 
-                ){}
             }
-            
+
         },
         actions = {
             TextButton(onClick = {}) {
@@ -98,38 +95,33 @@ fun MainAppBar(isDark: Boolean, onThemeChange: (Boolean) -> Unit) {
 
 
             Switch(
-                checked = isDark,
-                colors = SwitchDefaults.colors().copy(
-                    checkedThumbColor = Color.Transparent,
-                    uncheckedThumbColor = Color.Transparent,
-                ),
-                onCheckedChange = {
-                    scope.launch {
-                        onThemeChange(!isDark)
-                    }
-                }, thumbContent = {
-                    AnimatedVisibility(
-                        !isDark,
-                        enter = fadeIn(),
-                        exit = fadeOut()
-                    ) {
-                        Icon(
-                            Icons.Default.LightMode, "DarkMode",
-                            tint = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                    }
-                    AnimatedVisibility(
-                        isDark,
-                        enter = fadeIn(),
-                        exit = fadeOut()
-                    ) {
-                        Icon(
-                            Icons.Default.DarkMode, "DarkMode",
-                            tint = MaterialTheme.colorScheme.onPrimaryContainer
-                        )
-                    }
+                checked = isDark, colors = SwitchDefaults.colors().copy(
+                checkedThumbColor = Color.Transparent,
+                uncheckedThumbColor = Color.Transparent,
+            ), onCheckedChange = {
+                scope.launch {
+                    onThemeChange(!isDark)
                 }
-            )
+            }, thumbContent = {
+                AnimatedVisibility(
+                    !isDark, enter = fadeIn(), exit = fadeOut()
+                ) {
+                    Icon(
+                        Icons.Default.LightMode,
+                        "DarkMode",
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                }
+                AnimatedVisibility(
+                    isDark, enter = fadeIn(), exit = fadeOut()
+                ) {
+                    Icon(
+                        Icons.Default.DarkMode,
+                        "DarkMode",
+                        tint = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                }
+            })
         },
 
 
