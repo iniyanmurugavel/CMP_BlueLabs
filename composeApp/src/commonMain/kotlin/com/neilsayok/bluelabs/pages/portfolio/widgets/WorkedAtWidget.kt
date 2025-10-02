f2e6984037e469c071b7f8aeee58bf9d9d1d1f9f
NeilSayok <sayokdeymajumder1998@gmail.com>
2025-10-02 18:07:21 +0530

    Refactor WorkedAtWidget to simplify tab selection and improve logging

--- composeApp/src/commonMain/kotlin/com/neilsayok/bluelabs/pages/portfolio/widgets/WorkedAtWidget.kt
+++ composeApp/src/commonMain/kotlin/com/neilsayok/bluelabs/pages/portfolio/widgets/WorkedAtWidget.kt
@@ -14,11 +14,13 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -30,28 +32,32 @@ import com.neilsayok.bluelabs.data.portfolio.PortfolioFileContents
 import com.neilsayok.bluelabs.domain.util.Response
 import com.neilsayok.bluelabs.util.Log
 import com.neilsayok.bluelabs.util.loadImage
+import io.ktor.util.collections.getValue
+import io.ktor.utils.io.InternalAPI
 import kotlinx.coroutines.launch
 
 
+@OptIn(InternalAPI::class)
 @Composable
 fun WorkedAtWidget(
     jobs: List<PortfolioFileContents>, getJobsIcon: (PortfolioFileContents) -> String?
 ) {
 
-    Log.d("jobs", jobs.toString())
-
     val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState(pageCount = { jobs.size })
-    val selectedTabIndex = remember { derivedStateOf { pagerState.currentPage } }
+
+    Log.d("currentPage", pagerState.currentPage.toString())
+
 
     Column {
-        TabRow(selectedTabIndex = selectedTabIndex.value) {
+        TabRow(selectedTabIndex = pagerState.currentPage) {
             jobs.forEachIndexed { index, currentTab ->
                 Tab(
-                    selected = selectedTabIndex.value == index,
+                    selected = pagerState.currentPage == index,
                     onClick = {
                         scope.launch {
                             pagerState.animateScrollToPage(index)
+
                         }
                     },
                     text = {
@@ -74,17 +80,14 @@ fun WorkedAtWidget(
                     )
             }
         }
-        val response: PortfolioFileContents? by mutableStateOf( jobs.firstOrNull{it.order == (selectedTabIndex.value+1)})
-
-        Log.d("response", response.toString())
-        Log.d("selectedIndex", "${selectedTabIndex.value}")
 
         Card(shape = RoundedCornerShape(bottomEnd = 12.dp, bottomStart = 12.dp)) {
             HorizontalPager(
                 state = pagerState,
-            ) {
+            ) { page ->
+                val currentJobContent = jobs.firstOrNull { it.order == (page + 1) }?.content
                 Column {
-                    response?.content?.let { markdown -> MarkdownHandler(markdown) }
+                    currentJobContent?.let { markdown -> MarkdownHandler(markdown) }
                 }
 
             }
